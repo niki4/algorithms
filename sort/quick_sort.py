5dbe234848a065a92cbd6d287a0825fa681d7ae4
Ivan Nikiforov <nikiforr@gmail.com>
2021-05-14 15:33:26 +0300

    Refactor

--- sort/quick_sort.py
+++ sort/quick_sort.py
@@ -1,59 +1,51 @@
 import random
-import timeit
 
-# Runtime complexity: O(n log n) + O(k) because of slicing.
-# Extra memory for slicing and creating arrays.
-def qsort_simple(items):
-    if items:
-        pivot = items[0]
-        below = [i for i in items[1:] if i < pivot]
-        above = [j for j in items[1:] if j >= pivot]
-        return qsort_simple(below) + [pivot] + qsort_simple(above)
-    else:
+
+class Solution:
+    # Runtime complexity: O(n log n) + O(k) because of slicing.
+    # Extra memory for slicing and creating arrays.
+    def quicksort(self, items):
+        if items:
+            pivot = items[0]
+            below = [i for i in items[1:] if i < pivot]
+            above = [j for j in items[1:] if j >= pivot]
+            return self.quicksort(below) + [pivot] + self.quicksort(above)
+        else:
+            return items
+
+
+class Solution2:
+    # Using 'Hoare partition scheme' allows to do sort in place.
+    # https://en.wikipedia.org/wiki/Quicksort#Hoare_partition_scheme
+    def quicksort(self, items):
+        self._qsort(items, 0, len(items) - 1)
         return items
 
-# Using 'Hoare partition scheme' allows to do sort in place.
-# https://en.wikipedia.org/wiki/Quicksort#Hoare_partition_scheme
-def qsort_hoare(items):
-    def _qsort(items, low, high):
+    def _qsort(self, items, low, high):
         if low < high:
-            part = _partition(items, low, high)
-            _qsort(items, low, part)
-            _qsort(items, part + 1, high)
-    
-    def _partition(items, low, high):
-        pivot = items[(low + high) // 2]
+            part = self._partition(items, low, high)
+            self._qsort(items, low, part)
+            self._qsort(items, part + 1, high)
+
+    def _partition(self, items, low, high):
+        pivot = items[(low + high) // 2]  # pick mid element as a pivot
         while True:
             while items[low] < pivot:  # move left mark
                 low += 1
-            while items[high] > pivot: # move right mark
+            while items[high] > pivot:  # move right mark
                 high -= 1
-            if low >= high:            # found split point
+            if low >= high:  # found split point
                 return high
             items[low], items[high] = items[high], items[low]
             low += 1
             high -= 1
-    
-    _qsort(items, 0, len(items)-1)
-    return items
 
 
 if __name__ == '__main__':
-    to_sort1 = list(range(10))
-    to_sort2 = list(range(10))
+    solutions = [Solution(), Solution2()]
+    for sol in solutions:
+        to_sort = list(range(100))
+        random.shuffle(to_sort)
+        sorted_list = sol.quicksort(to_sort)
 
-    random.shuffle(to_sort1)
-    random.shuffle(to_sort2)
-
-    t1 = timeit.Timer('qsort_simple(to_sort1)', 
-        'from __main__ import qsort_simple, to_sort1')
-    t2 = timeit.Timer('qsort_hoare(to_sort2)',
-        'from __main__ import qsort_hoare, to_sort2')
-
-    print('qsort_simple rate:', t1.timeit())
-    print('qsort_hoare rate: ', t2.timeit())
-    
-    sorted1 = qsort_simple(to_sort1)
-    qsort_hoare(to_sort2)
-    
-    assert sorted1 == to_sort2 == list(range(10))
+        assert sorted_list == list(range(100))
